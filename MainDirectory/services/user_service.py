a3e8b96f798d6ec956ed7bc9d752d5d8ff3a4d78
Mwti <97027005+Mwteusz@users.noreply.github.com>
2024-11-11 19:29:33 +0100

    add test login and registration endpoints

--- MainDirectory/services/user_service.py
+++ MainDirectory/services/user_service.py
@@ -1,9 +1,35 @@
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 import MainDirectory.models.category as _category
+import jwt
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from datetime import datetime
+from pydantic import BaseModel
+
+
+SECRET_KEY = "secret_key" #TODO: hide !!
+security = HTTPBearer()
 
 class UserService:
 
     @staticmethod
     def get_test():
         return {" test " : "test"}
+    
+    @staticmethod
+    def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+        try:
+            token = credentials.credentials
+            payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            username = payload.get("sub")
+            
+            if username is None:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+            
+            return token
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
+        except jwt.PyJWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
