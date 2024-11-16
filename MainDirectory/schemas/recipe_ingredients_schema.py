ee7944fb594561034996e63802450d77870d4b7b
mylosz01 <240840@edu.p.lodz.pl>
2024-11-16 17:14:09 +0100

    Add RecipeIngredient schema

---
+++ MainDirectory/schemas/recipe_ingredients_schema.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+from MainDirectory.schemas.ingredient_schema import IngredientResponse
+
+class RecipeIngredientRequestAdd(BaseModel):
+    recipe_id : int
+    ingredient_id : int
+    quantity : float
+    unit : str
+
+
+class RecipeIngredientResponse(BaseModel):
+    recipe_id : int
+    ingredient : IngredientResponse
+    quantity : float
+    unit : str
+
+    class Config:
+        orm_mode = True
