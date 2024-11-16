5ff0461dc9f48b20d1eedad992b8f913cb76c566
mylosz01 <240840@edu.p.lodz.pl>
2024-11-16 18:00:51 +0100

    Implement method to add recipe ingredients

--- MainDirectory/services/recipe_ingridient_service.py
+++ MainDirectory/services/recipe_ingridient_service.py
@@ -1,7 +1,10 @@
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
+from typing import List
 
 from MainDirectory.services.recipe_service import RecipeService
+from MainDirectory.models.recipe_ingredients import RecipeIngredient
+from MainDirectory.schemas.recipe_ingredients_schema import RecipeIngredientRequestAdd
 from MainDirectory.models.recipe_ingredients import RecipeIngredient
 
 class RecipeIngredientService:
@@ -17,3 +20,22 @@ class RecipeIngredientService:
         stmt = _sql.select(RecipeIngredient).where(RecipeIngredient.recipe_id == _recipe_id)
         
         return _db.execute(stmt).scalars().all()
+    
+    @staticmethod
+    def add_recipe_ingredients(_recipe_ingredients : List[RecipeIngredientRequestAdd] , _db : _orm.Session):
+        
+        for recipe_ingredient in _recipe_ingredients:
+            #print(recipe_ingredient)
+
+            _new_recipe_ingredient = RecipeIngredient(
+                quantity = recipe_ingredient.quantity,
+                unit = recipe_ingredient.unit,
+                recipe_id = recipe_ingredient.recipe_id,
+                ingredient_id = recipe_ingredient.ingredient_id
+            )
+            
+
+            _db.add(_new_recipe_ingredient)
+            _db.commit()
+            _db.refresh(_new_recipe_ingredient)
+
