8d36be4e577e358220b135540659dd2237a7aaeb
mylosz01 <240840@edu.p.lodz.pl>
2024-11-17 16:39:45 +0100

    Implement method to add instruction step in recipe

--- MainDirectory/services/instruction_service.py
+++ MainDirectory/services/instruction_service.py
@@ -3,6 +3,8 @@ import sqlalchemy.orm as _orm
 from typing import List
 
 from MainDirectory.models.instruction_step import InstructionStep
+from MainDirectory.models.step import Step
+from MainDirectory.schemas.steps_schema import StepRequestResponse
 
 class InstructionService:
 
@@ -13,21 +15,27 @@ class InstructionService:
         
         return _db.execute(stmt).scalars().all()
     
-    # @staticmethod
-    # def add_recipe_ingredients(_recipe_ingredients : List[RecipeIngredientRequestAdd] , _db : _orm.Session):
-        
-    #     for recipe_ingredient in _recipe_ingredients:
-    #         #print(recipe_ingredient)
+    @staticmethod
+    def add_recipe_instruction(_recipe_id : int, _recipe_instructions : List[StepRequestResponse] , _db : _orm.Session):
+        #print(f"RECIPE ID = {_recipe_id}")
 
-    #         _new_recipe_ingredient = RecipeIngredient(
-    #             quantity = recipe_ingredient.quantity,
-    #             unit = recipe_ingredient.unit,
-    #             recipe_id = recipe_ingredient.recipe_id,
-    #             ingredient_id = recipe_ingredient.ingredient_id
-    #         )
+        for recipe_step in _recipe_instructions:
+            #print(recipe_step)
+
+            _new_step = Step(
+                name = recipe_step.name,
+                description = recipe_step.description,
+                step_number = recipe_step.step_number,
+                wait_time = recipe_step.wait_time
+            )
+
+            _new_recipe_step = InstructionStep(
+                recipe_id = _recipe_id,
+                step = _new_step
+            )
+
+            #print(f"STEP -> {_new_recipe_step}")
             
-
-    #         _db.add(_new_recipe_ingredient)
-    #         _db.commit()
-    #         _db.refresh(_new_recipe_ingredient)
-
+            _db.add( _new_recipe_step)
+            _db.commit()
+            _db.refresh( _new_recipe_step)
--- MainDirectory/services/recipe_ingridient_service.py
+++ MainDirectory/services/recipe_ingridient_service.py
@@ -2,7 +2,6 @@ import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 from typing import List
 
-from MainDirectory.services.recipe_service import RecipeService
 from MainDirectory.models.recipe_ingredients import RecipeIngredient
 from MainDirectory.schemas.recipe_ingredients_schema import RecipeIngredientRequestAdd
 from MainDirectory.models.recipe_ingredients import RecipeIngredient
@@ -18,7 +17,7 @@ class RecipeIngredientService:
     
     @staticmethod
     def add_recipe_ingredients(_recipe_ingredients : List[RecipeIngredientRequestAdd] , _db : _orm.Session):
-        
+
         for recipe_ingredient in _recipe_ingredients:
             #print(recipe_ingredient)
 
@@ -29,7 +28,6 @@ class RecipeIngredientService:
                 ingredient_id = recipe_ingredient.ingredient_id
             )
             
-
             _db.add(_new_recipe_ingredient)
             _db.commit()
             _db.refresh(_new_recipe_ingredient)
