bb651655827e14be4af91021cc7bf953ad06a898
mylosz01 <240840@edu.p.lodz.pl>
2024-11-27 22:52:14 +0100

    Make relationship between UserProgress and InstructionStep

--- app/models/user_progress.py
+++ app/models/user_progress.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Time, DateTime, ForeignKey
+from sqlalchemy import Integer, String, Time, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 from app.models import Base
@@ -8,13 +8,15 @@ class UserProgress(Base):
 
     user_progress_id : Mapped[int] = mapped_column(Integer, primary_key = True, index = True, autoincrement = True)
 
-    started_date : Mapped[DateTime] = mapped_column(DateTime)
+    started_date : Mapped[DateTime] = mapped_column(DateTime, nullable = True)
     finished_date : Mapped[DateTime] = mapped_column(DateTime, nullable = True)
-    current_step : Mapped[Integer] = mapped_column(Integer)
+    completed : Mapped[Boolean] = mapped_column(Boolean)
 
     # relationship with User - Recipe by UserProgress
-    recipe_id : Mapped[Integer] = mapped_column(Integer, ForeignKey('recipes.recipe_id'), primary_key = True)
-    user_id : Mapped[Integer] = mapped_column(Integer, ForeignKey('users.user_id'), primary_key = True)
+    recipe_id : Mapped[Integer] = mapped_column(Integer, ForeignKey('recipes.recipe_id'))
+    user_id : Mapped[Integer] = mapped_column(Integer, ForeignKey('users.user_id'))
+    instruction_steps_id : Mapped[Integer] = mapped_column(Integer, ForeignKey('instruction_steps.ingredient_step_id'))
 
     user : Mapped["User"] = relationship(back_populates = 'recipes')
     recipe : Mapped["Recipe"] = relationship(back_populates = 'users')
+    instruction_step : Mapped["InstructionStep"] = relationship(back_populates = 'user_progress')
